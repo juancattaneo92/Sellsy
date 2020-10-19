53669891de9a5e958b5f3a10452aeeeadee9e176
Juan <juancattaneo92@gmail.com>
2020-10-19 16:55:28 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +6,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Product.destroy_all
 
 User.create(email:'demoGuest@sellsy.com', f_name:'Demo Guest', password:'demopassword')
 
