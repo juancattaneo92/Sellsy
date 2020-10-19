1a7183f0262556625665ce65cd031b0c80271e4a
Juan <juancattaneo92@gmail.com>
2020-10-19 16:28:20 -0700

    More products

--- frontend/components/product/product_index_item.jsx
+++ frontend/components/product/product_index_item.jsx
@@ -3,17 +3,45 @@ import { Link } from 'react-router-dom';
 
 const ProductIndexItem = (props) => {
 let product = props.product
+
   return (
     <div className="grid-item">
-    <li className="individual-item">
-      <Link to={`/products/${product.id}`} />
-      <div className="image-holder">Image</div>
-      <div className="name-holder">{product.title}</div>
-      <div className="price-holder">${product.price}.00</div>
-    </li>
+      <Link to={`/products/${product.id}`} className="grid-item-link">
+        <li className="individual-item">
+          <div className="image-holder">Image</div>
+          <div className="name-holder">{product.title}</div>
+          <div className="price-holder">${product.price}.00</div>
+        </li>
+      </Link>
     </div>
+      
   )
 }
 
 export default ProductIndexItem;
 
+// class ProductIndexItem extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick() {
+//     this.props.history.push(`/products/${product.id}`);
+//   }
+
+//   render() {
+    
+//     return (
+//       < div className="grid-item" onClick={this.handleClick}>
+//         <li className="individual-item">
+//           <div className="image-holder">Image</div>
+//           <div className="name-holder">{this.props.product.title}</div>
+//           <div className="price-holder">${this.props.product.price}.00</div>
+//         </li>
+//     </div > 
+//     );
+//   }
+// }
+
+// export default ProductIndexItem;
